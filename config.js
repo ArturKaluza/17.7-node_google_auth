b04e1cf67c3e88cf18491ac7f8b02c9e18692511
Artur Kaluza <akaluza26@gmail.com>
2018-01-28 12:21:03 +0100

    Add functionality

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'GOOGLE_CLIENT_ID': '290569852075-kpnv6bv4s7d10e9artv83sa760j7u0d6.apps.googleusercontent.com',
+  'GOOGLE_CLIENT_SECRET': 'AJznRPMZYCPkO7Ocr__Ag1lm',
+  'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
